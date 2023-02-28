e561f09dac1b7dceb7693e2111bd40258e75a683
annagao <ggaoyanyan@gmail.com>
2023-02-28 13:01:44 -0800

    heartlist

---
+++ i-heart-react/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import HeartList from './components/HeartList';
+import './App.css';
+
+const message = 'cool cud';
+
+function App() {
+  return (<>
+  <Header/>
+  
+  <HeartList/>
+  </>
+  )
+}
+
+export default App;
---
+++ i-heart-react/src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = ()=>{
+    return(
+    <div className='App-header'>
+        <p>I ❤️ React</p>
+    </div>
+    )
+};
+
+export default Header;
